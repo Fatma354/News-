3c3a1808cf18a4225a793051c916596c7d6e2886
Fatma354 <fm6152566@gmail.com>
2025-09-30 10:33:08 +0300

    Update categories_listview.dart

--- lib/widget/categories_listview.dart
+++ lib/widget/categories_listview.dart
@@ -1,44 +1,4 @@
-/*import 'package:first/model/category_model.dart';
-import 'package:first/widget/category_card.dart';
-import 'package:flutter/material.dart';
 
-/*ويدجت بتمثل كارت للتصنيف.
-بتعرض صورة واسم.
-   (CategoryView)لو ضغطت عليها → بتفتح شاشة جديد
-   وتعرض الأخبار الخاصة بالتصنيف ده
-*/
-class CategoriesListview extends StatelessWidget {
-  const CategoriesListview({super.key});
-  final List<categoryModel> categories = const [
-    categoryModel(
-        image: 'assets/image/technology.jpeg',
-        name: /*'Technology'*/ 'Culture'),
-    categoryModel(image: 'assets/image/business.avif', name: 'business'),
-    categoryModel(
-        image: 'assets/image/entertaiment.avif', name: 'entertainment'),
-    categoryModel(image: 'assets/image/health.avif', name: 'health'),
-    categoryModel(image: 'assets/image/science.avif', name: 'science'),
-    categoryModel(image: 'assets/image/sports.avif', name: 'sports'),
-    categoryModel(image: 'assets/image/general.avif', name: 'genera'),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        height: 85,
-        // التمرير فيها بيكون افقي بس علشان استخدمنا
-        // الويدجيت في الستر التاني بتكون افقي
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
-            itemBuilder: (context, index) {
-              return CategoryCard(
-                category: categories[index],
-              );
-            }));
-  }
-}*/
-
-//tharwat
 import 'package:first/model/category_model.dart';
 import 'package:flutter/material.dart';
 import 'category_card.dart';
