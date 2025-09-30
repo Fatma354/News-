80f4c112665f6dd29bffeb87d6d114cebe8491d0
Fatma354 <fm6152566@gmail.com>
2025-09-30 10:31:12 +0300

    Update home_view.dart

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,58 +1,5 @@
-/*import 'package:first/widget/categories_listview.dart';
-import 'package:flutter/material.dart'; //{chatgpt}
-import 'package:first/widget/Newslistviewbuilder.dart';
-/*الشاشة الرئيسية للتطبيق.
-فيها AppBar مكتوب فيه "Newes Cloud".
-(body)→ بتعرض التصنيفات أفقياً. CategoriesListview عباره عن 
-NewsListViewBuilder(category: 'world') →"world" بيعرض الأخبار الخاصة بالـ .*/
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            'Newes',
-            style: TextStyle(
-              color: Colors.black87,
-            ),
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Text(
-            'Cloud',
-            style: TextStyle(
-              color: Colors.orange,
-            ),
-          ),
-        ]),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: CategoriesListview(),
-            ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: 22,
-              ),
-            ),
-            NewsListViewBuilder(category: 'world'),
-          ],
-        ),
-      ),
-    );
-  }
-}*/
-
-// tharwat
 import 'package:first/widget/Newslistviewbuilder.dart';
 import 'package:first/widget/categories_listview.dart';
 import 'package:flutter/material.dart';
