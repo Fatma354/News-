0cd91be426f0e83a2238777f3702ad4148029645
Fatma354 <fm6152566@gmail.com>
2025-09-30 10:30:28 +0300

    Update category_view.dart

--- lib/view/category_view.dart
+++ lib/view/category_view.dart
@@ -1,28 +1,4 @@
-/*import 'package:first/widget/Newslistviewbuilder.dart';
-import 'package:flutter/material.dart';
 
-/*شاشة بتعرض الأخبار الخاصة بتصنيف معين.
-CustomScrollView بتستخدم 
-و عشان تبني الأخبارNewsListViewBuilder وفيها*/
-class CategoryView extends StatelessWidget {
-  const CategoryView({super.key, required this.category});
-  final String category;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          //ده كدا للربط بين الكاتوجري
-          // والبيانات البادي الي تحت
-          NewsListViewBuilder(
-            category: category,
-          )
-        ],
-      ),
-    );
-  }
-}*/
-//tharwat
 import 'package:first/widget/Newslistviewbuilder.dart';
 import 'package:flutter/material.dart';
 
