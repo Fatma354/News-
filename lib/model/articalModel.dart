600075b0f105d4305057335bf410162cbabd5078
Fatma354 <fm6152566@gmail.com>
2025-09-30 10:28:42 +0300

    Update articalModel.dart

--- lib/model/articalModel.dart
+++ lib/model/articalModel.dart
@@ -1,25 +1,4 @@
-/*ده موديل (كائن) بيمثل المقالة.
-ليه 3 متغيرات: الصورة – العنوان – الوصف.*/
-/*class Articalmodel {
-  final String? image;
-  final String title;
-  final String? subtitle;
-//بنباصي ليه البيانات
-//(required لازم القيمه الي هتمر فيه متكونش فارغه)
-  Articalmodel(
-      {required this.image, required this.title, required this.subtitle});
-  //JSON → Objec عشان نسهّل التحويل من
-//APIs مع Flutter أغلب شغل
-// JSON بيكون بيانات راجعة على هيئة
-  factory Articalmodel.fromJson(json) {
-    return Articalmodel(
-      image: json["image_url"],
-      title: json["title"],
-      subtitle: json["description"],
-    );
-  }
-}*/
-//tharwat
+
 class ArticleModel {
   final String? image;
   final String? title;
